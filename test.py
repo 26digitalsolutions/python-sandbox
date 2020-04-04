ce8a50e27f0fbdc95ca97292323eb8f71fb9cf9b
Zaid <Zaid@mbp-de-zaid.home>
2020-04-04 17:51:02 +0100

    Test commit

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-def hello(name):
-    print(f"hello {name}")
+'''
+This is a test file!
+'''
 
-hello("zaid")
+class user():
+
+    planet = "earth"
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"User is {self.name} - Age is {self.age} - Lives on {self.planet}"
+
+user1 = user("Zaid",30)
+print(user1)
